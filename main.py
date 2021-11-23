a8d72f488f8d0fdeaaf4c7e6c76e750073856e35
DeFaltDev <85969333+DeFaltDev@users.noreply.github.com>
2021-11-23 08:41:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,10 @@ def red():
     RED = "\033[1;31m"
     stdout.write(RED)
 
-def green():
-    GREEN = "\033[0;32m"
-    stdout.write(GREEN)
-
 def blue():
     BLUE = "\033[1;34m"
     stdout.write(BLUE)
-
-def yellow():
-    YELLOW = "\033[1;33m"
-    stdout.write(YELLOW)
-
+    
 def purple():
     PURPLE = "\033[1;35m"
     stdout.write(PURPLE)
