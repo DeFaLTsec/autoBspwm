0b15d043d23ef4e8da63818e60b4a0865ac369f5
DeFaltDev <85969333+DeFaltDev@users.noreply.github.com>
2021-11-23 09:04:58 -0500

    Delete files directory

--- files/configbin/target_to_hack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-target=$(cat ~/.config/bin/target)
-
-if [ $target ]; then
-    echo "%{F#24c3cc}什%{F#ffffff} $target%{u-}"
-else
-    echo "%{F#24c3cc}ﲅ %{u-}%{F#ffffff} No target"
-fi
